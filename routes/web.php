c89a30ffd60e4b9df175be6a8ba107fd80d4d11e
Azizbek Hakimov <azizxakimov45@gmail.com>
2026-04-30 08:40:10 +0500

    add PageController and define web routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\View\View;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PageController::class, 'home']);
+Route::get('/about', [PageController::class, 'about']);
+Route::get('/services', [PageController::class, 'services']);
+Route::get('/blog', [BlogController::class, 'index']);
+Route::get('/blog/{page}', [BlogController::class, 'view']);
+Route::get('/contact', [PageController::class, 'contact']);
